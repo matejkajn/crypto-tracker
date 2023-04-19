d5f84653266e77f0126099cc4579260af9b0cff9
matejkajn <matejka.jn@gmail.com>
2023-04-19 19:31:50 +0200

    fix: navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,25 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="w-full px-[5%] m-auto flex items-center justify-between shadow-md">
       <div className="p-2 font-extrabold">
-        <h1 className="text-3xl">CryptoTracker</h1>
+        <Link to="/">
+          {" "}
+          <h1 className="text-3xl">CryptoTracker</h1>
+        </Link>
       </div>
       <ul className="flex font-semibold">
-        <li className="text-xl m-4 hover:text-[#6188ff] cursor-pointer">
+        <li className="text-xl m-4 hover:text-indigo-500 cursor-pointer">
           <NavLink to="/">Home</NavLink>
         </li>
-        <li className="text-xl m-4 hover:text-[#6188ff] cursor-pointer">
+        <li className="text-xl m-4 hover:text-indigo-500 cursor-pointer">
           <NavLink to="/coins">Coins</NavLink>
         </li>
-        <li className="text-xl m-4 hover:text-[#6188ff] cursor-pointer">
+        <li className="text-xl m-4 hover:text-indigo-500 cursor-pointer">
           <NavLink to="/news">News</NavLink>
         </li>
-        <li className="text-xl m-4 hover:text-[#6188ff] cursor-pointer">
+        <li className="text-xl m-4 hover:text-indigo-500 cursor-pointer">
           <NavLink to="/about">About</NavLink>
         </li>
       </ul>
