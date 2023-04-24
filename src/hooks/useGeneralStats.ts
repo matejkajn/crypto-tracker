9b127a38fa3bb53beee8aaca407c5dcf0602c9e6
matejkajn <matejka.jn@gmail.com>
2023-04-24 16:55:20 +0200

    feat: api hook for general stats

---
+++ src/hooks/useGeneralStats.ts
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const options = {
+    method: 'GET',
+    url: 'https://coingecko.p.rapidapi.com/global',
+    headers: {
+      'content-type': 'application/octet-stream',
+      'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+      'X-RapidAPI-Host': import.meta.env.VITE_RAPID_API_HOST
+    }
+  };
+
+export const useGeneralStats = <GeneralStats>():[boolean, GeneralStats | undefined, any] => {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [data, setData] = useState<GeneralStats>()
+    const [error, setError] = useState<any>()
+
+    useEffect(() => {
+        sendRequest()
+    }, [])
+
+    const sendRequest = () => {
+        setLoading(true)
+
+        axios(options)
+            .then((resp) => {
+                setData(resp.data)
+            })
+            .catch((err) => {
+                setError(err)
+            })
+            .finally(() => setLoading(false))
+    }
+
+    return  [loading, data, error ]
+}
