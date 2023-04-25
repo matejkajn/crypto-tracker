996779a0c39db61cae321c02751d4c57f8c7b6d2
matejkajn <matejka.jn@gmail.com>
2023-04-25 23:13:22 +0200

    feat: dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,71 @@
+import { useGeneralStats } from "../hooks/useGeneralStats";
+
+import { GeneralStats } from "../types/types";
+
+import DashboardBlock from "./DashboardBlock";
+
+import { CircleStackIcon } from "@heroicons/react/24/solid";
+import { ShoppingBagIcon } from "@heroicons/react/24/solid";
+import { BanknotesIcon } from "@heroicons/react/24/solid";
+import { ClockIcon } from "@heroicons/react/24/solid";
+
+import millify from "millify";
+
+const Dashboard = () => {
+  const [loading, data, error] = useGeneralStats<GeneralStats>();
+
+  if (error) return <div>Nějaký error</div>;
+
+  let sumMarketCap = 0;
+
+  for (const key in data?.data.total_market_cap) {
+    sumMarketCap = sumMarketCap + data?.data.total_market_cap[key];
+  }
+
+  const marketCapPercentageArray = [];
+  for (const key in data?.data.market_cap_percentage) {
+    marketCapPercentageArray.push({
+      name: key,
+      value: data?.data.market_cap_percentage[key],
+    });
+  }
+
+  return (
+    <div className="m-5">
+      <div className="text-indigo-500 font-bold text-4xl text-center p-10">
+        General crypto stats
+      </div>
+      <div className="flex flex-wrap">
+        <DashboardBlock
+          title="Active Cryptos"
+          value={data?.data?.active_cryptocurrencies}
+          icon={<CircleStackIcon />}
+          loading={loading}
+        />
+        <DashboardBlock
+          title="Markets"
+          value={data?.data?.markets}
+          icon={<ShoppingBagIcon />}
+          loading={loading}
+        />
+        <DashboardBlock
+          title="Market Cap"
+          value={Math.round((sumMarketCap as number) / 1000000000)}
+          icon={<BanknotesIcon />}
+          loading={loading}
+        />
+        <DashboardBlock
+          title="24h Vol"
+          value={millify(
+            data?.data?.market_cap_change_percentage_24h_usd as number,
+            { precision: 6 }
+          )}
+          icon={<ClockIcon />}
+          loading={loading}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import Dashboard from "../components/Dashboard";
 import BaseLayout from "../layouts/BaseLayout";
 
 const HomePage = () => {
   return (
     <BaseLayout>
-      <h1>Homepage</h1>
+      <div className="w-3/4 m-auto">
+        <Dashboard />
+      </div>
     </BaseLayout>
   );
 };
