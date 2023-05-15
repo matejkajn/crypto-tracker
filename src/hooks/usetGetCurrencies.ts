0b1d1949a13e2cb9144665a46610b20293f742d2
matejkajn <matejka.jn@gmail.com>
2023-05-15 17:43:26 +0200

    feat: currency hook

---
+++ src/hooks/usetGetCurrencies.ts
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const options = {
+  method: 'GET',
+  url: import.meta.env.VITE_RAPID_API_CURRENCIES_URL,
+  params: {
+    limit: '60',
+  },
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+    'X-RapidAPI-Host': import.meta.env.VITE_RAPID_API_HOST
+  }
+};
+
+export const useGetCurrencies = <Currencies>():[boolean, Currencies | undefined, any] => {
+    const [loading, setLoading] = useState<boolean>(true)
+    const [data, setData] = useState<Currencies>()
+    const [error, setError] = useState<any>()
+
+    useEffect(() => {
+      sendRequest()
+  }, [])
+
+  const sendRequest = () => {
+      setLoading(true)
+
+      axios(options)
+          .then((resp) => {
+              setData(resp.data)
+          })
+          .catch((err) => {
+              setError(err)
+          })
+          .finally(() => setTimeout(() => setLoading(false), 1500))
+  }
+
+  return [loading, data, error ]
+}
