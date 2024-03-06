f7d0647c67de6c91b3fad9af5d9145bdb912c3e1
matejkajn <matejka.jn@gmail.com>
2024-03-06 09:34:55 +0100

    refactor: welcome page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,12 @@ import BaseLayout from "../layouts/BaseLayout";
 const HomePage = () => {
   return (
     <BaseLayout>
+      <div className="mx-auto my-5 flex">
+        <img src="src/assets/logo.png" width={600} />
+        <h1 className="font-bold text-6xl text-right justify-start p-10">
+          Welcome to my CryptoTracker app.
+        </h1>
+      </div>
       <div className="w-3/4 m-auto">
         <Dashboard />
       </div>
