7e6724afa2c2a5f928d5021b45fde8fb40eb077e
matejkajn <matejka.jn@gmail.com>
2024-04-10 14:49:36 +0200

    feat: add news page

--- src/components/NewsTable.tsx
+++ src/components/NewsTable.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useGetNews } from "../hooks/useGetNews";
 import { CoinNews } from "../types/types";
 import Loader from "./Loader";
@@ -8,15 +9,21 @@ const NewsTable = () => {
 
   if (error) throw new Error("There was a problem while obtaining news data.");
 
-  const newsPerPage = 6;
+  const [newsOnPage, setNewsOnPage] = useState<number>(6);
+  const loadMore = () => {
+    setNewsOnPage(newsOnPage + 6);
+  };
+
+  const parsedData = data?.data.slice(0, newsOnPage);
 
   return (
     <>
       <div className="container my-24 mx-auto md:px-6">
         <section className="mb-32 text-center">
           <div className="grid gap-6 lg:grid-cols-3 xl:gap-x-12">
-            {data?.data.map((item) => (
+            {parsedData?.map((item) => (
               <NewItem
+                key={item.url}
                 url={item.url}
                 title={item.title}
                 description={item.description}
@@ -27,6 +34,17 @@ const NewsTable = () => {
             ))}
           </div>
         </section>
+        {data?.data.length != parsedData?.length && (
+          <div className="text-center">
+            <button
+              type="button"
+              onClick={loadMore}
+              className="text-white bg-indigo-500 hover:bg-indigo-800 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2"
+            >
+              Load more
+            </button>
+          </div>
+        )}
       </div>
       {loading && <Loader />}
     </>
