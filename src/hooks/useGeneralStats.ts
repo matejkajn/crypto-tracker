f6d9573dc19dcdfae9cd4e1e11193e253ae3be1a
matejkajn <matejka.jn@gmail.com>
2024-04-10 14:50:25 +0200

    refactor: remove white space

--- src/hooks/useGeneralStats.ts
+++ src/hooks/useGeneralStats.ts
@@ -1,14 +1,14 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-  const options = {
-    method: 'GET',
-    url: import.meta.env.VITE_RAPID_API_GLOBAL_STATS_URL,
-    headers: {
-      'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
-      'X-RapidAPI-Host': import.meta.env.VITE_RAPID_API_HOST
-    }
-  };
+const options = {
+  method: 'GET',
+  url: import.meta.env.VITE_RAPID_API_GLOBAL_STATS_URL,
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+    'X-RapidAPI-Host': import.meta.env.VITE_RAPID_API_HOST
+  }
+};
 
 export const useGeneralStats = <GeneralStats>():[boolean, GeneralStats | undefined, any] => {
     const [loading, setLoading] = useState<boolean>(true)
